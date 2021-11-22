9f5f82e5a957a8974f61ba02e9c5206b814cc0d9
Trịnh Hoàng Long <trinhhoanglong200@gmail.com>
2021-11-22 22:48:07 +0700

    add Teacher and Student auth

--- api/classesaccount/classesaccountController.js
+++ api/classesaccount/classesaccountController.js
@@ -3,7 +3,7 @@ const poolean = require("../../Database/index.js");
 exports.GetAllAccount = async (req, res, user) => {
   
   const id = req.params.id;
- 
+  console.log("HEHE boiz")
   //Validate if the user is exist in the class
 
   
@@ -24,10 +24,41 @@ exports.GetAllAccount = async (req, res, user) => {
           success:false
         });
         return
-
     }
     res.header({ "Access-Control-Allow-Origin": "*" });
     res.status(200).json({ data: data.rows, success: true });
+  } catch (err) {
+    res.header({ "Access-Control-Allow-Origin": "*" });
+    res.status(404).json({
+      message: "No class available",
+      success:false
+    });
+  }
+};
+
+exports.checkTeacher = async (req, res, user) => {
+  
+  const id = req.params.id;
+  //Validate if the user is exist in the class
+  
+  try {
+    const type = await poolean.query(
+      'SELECT * FROM classesaccount where (classesaccount.classid=$1 AND classesaccount.accountid=$2 ) ',
+      [id,user.id]
+    );
+    //
+ 
+    
+    if (type.rows.length==0){
+        //this account not belong to this class
+        res.status(404).json({
+          message: "No class available",
+          success:false
+        });
+        return
+    }
+    res.header({ "Access-Control-Allow-Origin": "*" });
+    res.status(200).json({ data: type.rows, success: true });
   } catch (err) {
     res.header({ "Access-Control-Allow-Origin": "*" });
     res.status(404).json({
