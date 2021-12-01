b40b6a7e3e0ec2f3dd582e16aec8cabc2e2eda87
Trịnh Hoàng Long <trinhhoanglong200@gmail.com>
2021-12-01 15:38:23 +0700

    add upload grades

--- api/Grade/gradeController.js
+++ api/Grade/gradeController.js
@@ -3,24 +3,113 @@ const { v4: uuidv4 } = require("uuid");
 exports.GetAllGrade = async (req, res, user) => {
   //const id = req.params.id;
   //Validate if the user is exist in the class
-    
+
   try {
-    
     //
-    const data = await poolean.query(`
+    const data = await poolean.query(
+      `
     SELECT "Classes".name, "Classes".section, "Homework".name as homeworkName, grade.grade, grade.idaccount, grade.idclass,grade.idhomework
     FROM ((grade
     INNER JOIN "Classes" ON "Classes".id = grade.idclass)
     INNER JOIN "Homework" ON "Homework".id = grade.idhomework)
     WHERE grade.idaccount = $1;
-    `,[user.id])
+    `,
+      [user.id]
+    );
     res.header({ "Access-Control-Allow-Origin": "*" });
     res.status(200).json({ data: data.rows, success: true });
   } catch (err) {
     res.header({ "Access-Control-Allow-Origin": "*" });
     res.status(404).json({
       message: "No grade available",
-      success:false
+      success: false,
+    });
+  }
+};
+exports.GetAllGradeFromClass = async (req, res, user) => {
+  const id = req.params.id;
+  console.log(id);
+  //Validate if the user is exist in the class
+
+  try {
+    //
+    const data = await poolean.query(
+      `
+    SELECT * from
+    GRADE
+    WHERE grade.idclass = $1;
+    `,
+      [id]
+    );
+    res.header({ "Access-Control-Allow-Origin": "*" });
+    res.status(200).json({ data: data.rows, success: true });
+  } catch (err) {
+    res.header({ "Access-Control-Allow-Origin": "*" });
+    res.status(404).json({
+      message: "No grade available",
+      success: false,
+    });
+  }
+};
+exports.UpdateAllGradeFromClass = async (data, res) => {
+  try {
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+
+      const COUNT = await poolean.query(
+        `
+        SELECT COUNT(*) 
+        FROM GRADE 
+        WHERE idaccount = $1 AND idclass =$2 AND idhomework =$3
+        `,
+        [element.idaccount, element.idclass, element.idhomework]
+      );
+      if (COUNT.rows[0].count == 0) {
+        //new
+        await poolean.query(
+          `
+        INSERT INTO GRADE (idaccount, idclass, idhomework,grade)
+        VALUES ($1, $2, $3,$4)
+        RETURNING *
+        `,
+          [
+            element.idaccount,
+            element.idclass,
+            element.idhomework,
+            element.grade,
+          ]
+        );
+      } else {
+        //kiem tra thang do co trong lop do khong
+        const count = await poolean.query(
+          `
+          SELECT COUNT(*) 
+          FROM "classesaccount" 
+          WHERE accountid = $1 AND classid =$2 
+          `,
+          [element.idaccount, element.idclass]
+        );
+        
+        if (count.rows[0].count !== 0) {
+          await poolean.query(
+            `UPDATE GRADE  
+            SET grade=$1
+            WHERE idhomework=$2 AND idaccount=$3 `,
+            [element.grade, element.idhomework, element.idaccount]
+          );
+        }
+      }
+    }
+
+    //
+
+    res.header({ "Access-Control-Allow-Origin": "*" });
+    res.status(200).json({ data: data.rows, success: true });
+  } catch (err) {
+    res.header({ "Access-Control-Allow-Origin": "*" });
+    res.status(404).json({
+      message: "No grade available",
+      success: false,
     });
   }
 };
--- api/Grade/index.js
+++ api/Grade/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gradeController = require("./gradeController");
+
 const poolean = require("../../Database/index.js");
 
 const passport = require("../../authentication/index");
@@ -25,6 +26,48 @@ router.get("/", function (req, res, next) {
       }
     )(req, res, next);
   });
-
+  router.get("/getAllGradeFromClass/:id", function (req, res, next) {
+    passport.authenticate(
+      "jwt",
+      {
+        session: false,
+      },
+      function (err, user, info) {
+        
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          res.header({ "Access-Control-Allow-Origin": "*" });
+          res.status(401);
+          res.send({ message: info.message, success: false });
+          return;
+        }
+        gradeController.GetAllGradeFromClass(req, res,user);
+      }
+    )(req, res, next);
+  });
+  router.post("/UpdateGrades", function (req, res, next) {
+    passport.authenticate(
+      "jwt",
+      {
+        session: false,
+      },
+      function (err, user, info) {
+        
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          res.header({ "Access-Control-Allow-Origin": "*" });
+          res.status(401);
+          res.send({ message: info.message, success: false });
+          return;
+        }
+        gradeController.UpdateAllGradeFromClass(req.body.data,res);
+      }
+    )(req, res, next);
+  });
+  
 
   module.exports = router;
